1e55b8c026518490f3f55b00929f0a53d284ba3b
SilverWingedSeraph <evessarudem@gmail.com>
2015-08-17 08:29:04 -0700

    Add importance values.

--- SNSPClientServer.py
+++ SNSPClientServer.py
@@ -5,7 +5,18 @@ import struct
 import json
 from ipaddress import ip_address
 
+
 # TODO: Add IPv6
+
+# Importance values, 0 to 7 from least to most likley to require notifying the user.
+IMPORTANCE_ROUTINE = 0 # Messages for network testing, timed updates, etc. Should never be shown to the user.
+IMPORTANCE_UTILITY = 1 # Messages for utility purpouses, e.g., SNSP ping
+IMPORTANCE_CONFIG = 2  # Messages that might result in network or application configuration changes, but that do not announce a new service 
+IMPORTANCE_DEFAULT = 3 # Default. For messages announcing services.
+IMPORTANCE_NETANNOUNCE = 4 # Messages about how the network is configured that do NOT require user intervention (i.e., inform devices about optional use of OSPF)
+IMPORTANCE_SERVICESTOP = 5 # Messages about cessation of network services - both temporarily and permanently.
+IMPORTANCE_USERACTION = 6  # Messages that require user interaction - i.e., revealing captive portal, NETWORK DOWN IN..., etc.
+IMPORTANCE_VITAL = 7	   # Messages that absolutely positively must arrive and be shown to the user, if relevant. Possibly place in syslog too.
 
 VERSION = "0.01"
 
@@ -13,8 +24,10 @@ PORT = 81
 
 SOURCE_IP4 = False # False means use the default interface
 SITE_LOCAL_IP4 = "<broadcast>"
+SOURCE_IP6 = False
+SITE_LOCAL_IP6 = "" #TODO: Correct this
 
-def NewSNSPMessage(service_name, service_port, host_addr, message, version = VERSION):
+def NewSNSPMessage(service_name, service_port, host_addr, message, importance = IMPORTANCE_DEFAULT, version = VERSION):
 	"""
 	Create a new SNSP message.
 	"""
@@ -27,11 +40,15 @@ def NewSNSPMessage(service_name, service_port, host_addr, message, version = VER
 	if not isinstance(service_port, int):
 		raise ValueError("service_port was not an integer.")
 		
+	if (not type(importance) is int) or not ((importance >= IMPORTANCE_ROUTINE) and (importance <= IMPORTANCE_VITAL)):
+		raise ValueError("Invalid importance value.")
+		
 	try:
 		ip_address(host_addr) # If the address is not really an address, this will fail.
 	except ValueError:
 		raise ValueError("Address was not a valid IP address.")	
-	messagedict = {'version' : version, 'service_name' : service_name, 'service_port' : service_port, 'host_addr' : host_addr, 'message' : message} # TODO: Finish this!!
+	messagedict = {'version' : version, 'service_name' : service_name, \
+	'service_port' : service_port, 'host_addr' : host_addr, 'message' : message}
 	return messagedict
 	
 def set_network(source_ip, dest_ip):
